3c8ad68ac10f5f7f5ddffc1bed6f97f1cf9e6d4b
yulia <yuliia.serhienko@gmail.com>
2020-06-25 19:28:28 +0300

    Update activity app

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import moment from 'moment'
 
 Vue.config.productionTip = false
+
+Vue.filter('prettyTime', function (timeStamp) {
+  if (!timeStamp) return ''
+
+  return moment(timeStamp).format('LLL')
+
+})
 
 new Vue({
   render: h => h(App),
